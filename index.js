dac87cb59da84bd47a5e3f585de5877b6d338142
Sukanto Saha <sukanto113@gmail.com>
2025-06-20 20:34:02 +0600

    update port logic

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const http = require("http");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
